e86ed11fe35b24c1dff20b8e2bf2bdc32bc7658d
MGylden <99180585+MGylden@users.noreply.github.com>
2022-10-07 15:38:27 +0200

    updates services
    Services updated with:
    updateCustomer
    deleteCustomer

--- src/main/java/com/postgre/services/UserService.java
+++ src/main/java/com/postgre/services/UserService.java
@@ -1,5 +1,6 @@
 package com.postgre.services;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +12,17 @@ import com.postgre.repositories.UserRepository;
 @Service
 public class UserService {
 
-
 	@Autowired
 	private UserRepository userRepo;
-	
-	public ResponseEntity<User> getUserById(Long id){
+
+	public ResponseEntity<User> getUserById(Long id) {
 		HttpStatus httpStatus = null;
 		User returnUser = null;
-		
+
 		try {
-			if((returnUser = userRepo.getReferenceById(id)) != null) {
+			if ((returnUser = userRepo.getReferenceById(id)) != null) {
 				httpStatus = HttpStatus.OK;
-			}else {
+			} else {
 				httpStatus = HttpStatus.BAD_REQUEST;
 			}
 		} catch (Exception e) {
@@ -32,16 +32,16 @@ public class UserService {
 		}
 		return new ResponseEntity<>(returnUser, httpStatus);
 	}
-	
-	public ResponseEntity<User> createUser(User newUser){
+
+	public ResponseEntity<User> createUser(User newUser) {
 		HttpStatus httpStatus = null;
 		User returnUser = null;
-		
+
 		try {
-			if(userRepo.existByMail()==false) {
-				httpStatus = HttpStatus.OK;
+			if (userRepo.existByMail() == false) {
 				returnUser = userRepo.saveAndFlush(newUser);
-			}else {
+				httpStatus = HttpStatus.OK;
+			} else {
 				httpStatus = HttpStatus.BAD_REQUEST;
 			}
 		} catch (Exception e) {
@@ -50,5 +50,44 @@ public class UserService {
 			System.out.println(e.getMessage());
 		}
 		return new ResponseEntity<>(returnUser, httpStatus);
+	}
+
+	public ResponseEntity<User> deleteUser(Long id) {
+		HttpStatus httpStatus = null;
+		User returnUser = null;
+
+		try {
+			if((returnUser = userRepo.getReferenceById(id)) != null) {
+				userRepo.deleteById(id);
+				httpStatus = HttpStatus.OK;
+			}else {
+				httpStatus = HttpStatus.BAD_GATEWAY;
+			}
+		} catch (Exception e) {
+			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+			System.out.println("House is on fire.");
+			System.out.println(e.getMessage());
+		}
+		return new ResponseEntity<>(returnUser, httpStatus);
+	}
+	
+	public ResponseEntity<User> updateUser(User newUser){
+		HttpStatus httpStatus = null;
+		User returnUser = null;
+		
+		try {
+			if((returnUser = userRepo.getReferenceById(null))!= null) {
+				newUser = (User)HelperService.partialUpdate(httpStatus, returnUser);
+				returnUser = userRepo.saveAndFlush(newUser);
+				httpStatus = HttpStatus.OK;
+			}else {
+				httpStatus = HttpStatus.BAD_GATEWAY;
+			}
+		} catch (BeansException e) {
+			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+			System.out.println("House is on fire.");
+			System.out.println(e.getMessage());		}
+		return new ResponseEntity<>(returnUser, httpStatus);
+
 	}
 }
