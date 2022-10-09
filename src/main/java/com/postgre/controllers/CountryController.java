a14ab782acf45ad1fc2db965b723c822975f6b7e
MGylden <mathias@gylden.net>
2022-10-09 21:09:31 +0200

    patch 0.1.1

--- src/main/java/com/postgre/controllers/CountryController.java
+++ src/main/java/com/postgre/controllers/CountryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.postgre.models.Country;
-import com.postgre.models.Shipments;
 import com.postgre.services.CountryService;
 
 @RestController
--- src/main/java/com/postgre/controllers/UserController.java
+++ src/main/java/com/postgre/controllers/UserController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.postgre.models.Shipments;
 import com.postgre.models.User;
 import com.postgre.services.UserService;
-
-import lombok.*;
 
 @RestController
 @RequestMapping(path = "api/v1/Users")
