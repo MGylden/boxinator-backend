267db992b79ebef7835828b2d4b3c4d50a688b75
MGylden <99180585+MGylden@users.noreply.github.com>
2022-10-12 00:32:34 +0200

    Update UserRepository.java

--- src/main/java/com/postgre/repositories/UserRepository.java
+++ src/main/java/com/postgre/repositories/UserRepository.java
@@ -1,11 +1,8 @@
 package com.postgre.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< Updated upstream:src/main/java/com/postgre/repositories/UserRepository.java
-=======
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.CrudRepository;
->>>>>>> Stashed changes:src/main/java/com/postgre/demo/repositories/UserRepository.java
+
 import org.springframework.stereotype.Repository;
 
 import com.postgre.models.User;
@@ -14,8 +11,4 @@ import com.postgre.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-<<<<<<< Updated upstream:src/main/java/com/postgre/repositories/UserRepository.java
-	boolean existByMail();
-=======
->>>>>>> Stashed changes:src/main/java/com/postgre/demo/repositories/UserRepository.java
 }
